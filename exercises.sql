e89db74944b5d8c67c42291205890fd138368b95
ufuk <ufuk.uysl5@gmail.com>
2020-01-21 21:47:27 +0100

    List of most populated city

--- exercises.sql
+++ exercises.sql
@@ -3,3 +3,13 @@ SELECT name
 FROM world.city 
 WHERE CountryCode="IND"
 Order by Population desc limit 1
+
+--List the names of the countries having at least 3 cities with 3 million inhabitants.
+
+SELECT co.Name ,count(*) as cityCount 
+FROM world.city ci
+JOIN world.country co
+ON ci.CountryCode=co.Code
+WHERE ci.Population>3000000
+Group by co.name 
+having cityCount >=3
